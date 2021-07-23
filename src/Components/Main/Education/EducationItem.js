4b122d30700794b64b2d3c97f3ba077f7ae6993b
yamchinsky <66372922+yamchinsky@users.noreply.github.com>
2021-07-23 18:09:54 +0300

    changes

--- src/Components/Main/Education/EducationItem.js
+++ src/Components/Main/Education/EducationItem.js
@@ -3,27 +3,27 @@ import { EducationStyledItem } from "./EducationStyledItem";
 
 const EducationItem = () => {
   return (
-    <EducationStyledItem class="education-container">
-      <h3 class="education-title">Education:</h3>
-      <div class="education-school">
-        <h4 class="education-name">GoIT</h4>
-        <p class="education-specialization"> Fullstack developer</p>
+    <EducationStyledItem className="education-container">
+      <h3 className="education-title">Education:</h3>
+      <div className="education-school">
+        <h4 className="education-name">GoIT</h4>
+        <p className="education-specialization"> Fullstack developer</p>
         <p>
-          <span class="education-period">
-            05.2020-08.2021 <span class="education-devider"></span>Ukraine
+          <span className="education-period">
+            05.2020-08.2021 <span className="education-devider"></span>Ukraine
           </span>
         </p>
       </div>
       <div>
-        <h4 class="education-name">
+        <h4 className="education-name">
           National university of life and environmental sciences of Ukraine
         </h4>
-        <p class="education-specialization">
+        <p className="education-specialization">
           {" "}
           Master degree, Food Technology and quality control{" "}
         </p>
         <p>
-          <span class="education-period">09.2009-12.2010 Ukraine</span>
+          <span className="education-period">09.2009-12.2010 Ukraine</span>
         </p>
       </div>
     </EducationStyledItem>
