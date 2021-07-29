6587340fe7f603878fe46a26c474144b7380a1d4
yamchinsky <66372922+yamchinsky@users.noreply.github.com>
2021-07-29 10:36:55 +0300

    changes

--- src/Components/Main/Summary/SummaryItem.js
+++ src/Components/Main/Summary/SummaryItem.js
@@ -6,11 +6,11 @@ const SummaryItem = () => {
     <SummaryStyledItemContainer className="goal-container">
       <h2 className="goal-title">Summary:</h2>
       <p className="goal-text">
-        I am a FullStack developer and i am looking for a full-time position. I
+        I am a FullStack developer and I am looking for a full-time position. I
         have knowledge of HTML, CSS, SCSS, JS, React, Redux etc(more in
-        TechSkills chapter). Also i have hands-on experience in Agile/Scrum
-        methodologies when working in a team. My main goal is to development in
-        programming. I am very responsible and ready for new challenging tasks.
+        TechSkills chapter). I have hands-on experience in Agile/Scrum
+        methodologies. My main goal is development in programming. I am very
+        responsible and ready for new challenging tasks.
       </p>
     </SummaryStyledItemContainer>
   );
