6264dc3a708a30a4aeeac17e16da00f16608822b
yamchinsky <66372922+yamchinsky@users.noreply.github.com>
2021-07-30 09:45:34 +0300

    changes

--- src/Components/Main/Summary/SummaryItem.js
+++ src/Components/Main/Summary/SummaryItem.js
@@ -7,7 +7,7 @@ const SummaryItem = () => {
       <h2 className="goal-title">Summary:</h2>
       <p className="goal-text">
         I am a FullStack developer and I am looking for a full-time position. I
-        have knowledge of HTML, CSS, SCSS, JS, React, Redux etc(more in
+        have knowledge of HTML, CSS, SCSS, JS, React, Redux etc(see more in
         TechSkills chapter). I have hands-on experience in Agile/Scrum
         methodologies. My main goal is development in programming. I am very
         responsible and ready for new challenging tasks.
